daaa5750e2b4fcc1258921da95c60eb918bc1532
opti213 <opti213@mail.ru>
2018-12-04 12:10:58 +0300

    plus practice

---
+++ java/practice/src/ru/mirea/gotbachev/practice/practice01/task01/Main.java
@@ -0,0 +1,8 @@
+package ru.mirea.gorbachev.practice.practice01.task01;
+
+public class Main {
+    public static void main(String[] args) {
+        Ball b = new Ball(5);
+        System.out.println(b);
+    }
+}
---
+++ java/practice/src/ru/mirea/gotbachev/practice/practice03/task02/Movable.java
@@ -0,0 +1,8 @@
+package ru.mirea.gorbachev.practice.practice03.task02;
+
+public interface Movable {
+    void moveUp();
+    void moveDown();
+    void moveLeft();
+    void moveRight();
+}
